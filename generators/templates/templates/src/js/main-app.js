6cd6f53625e3a96e38ab9b720a4f5f91321273b9
Sahil Chinoy <sahil.chinoy@gmail.com>
2017-11-14 22:59:52 -0800

    add slugify to JS. fixes #4

--- generators/templates/templates/src/js/main-app.js
+++ generators/templates/templates/src/js/main-app.js
@@ -13,3 +13,14 @@ window.$('.icon-twitter').click((e) => {
   const status = encodeURIComponent(`${window.tweetText} ${uri}`);
   window.open(`https://twitter.com/home?status=${status}`);
 });
+
+// https://gist.github.com/mathewbyrne/1280286
+function slugify(text)
+{
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
